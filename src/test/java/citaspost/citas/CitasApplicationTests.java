84e3a2f4fcec97c2f6e37d19bf46fce2eb89356f
diegoacentenoo <diegoacentenoo12@gmail.com>
2026-09-15 18:28:33 -0400

    primer commit

---
+++ src/test/java/citaspost/citas/CitasApplicationTests.java
@@ -0,0 +1,13 @@
+package citaspost.citas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
